d4e0a6ac177f15b6768f032113a2ca1a0aae67a2
Pedro Henrique <83087869+PeedroHenriquee@users.noreply.github.com>
2023-04-13 15:29:20 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,9 @@
+//1. Faça um programa que tem um array de inteiros, calcule e mostre:
+   // 2. a quantidade de números;
+
+import Foundation
+
+let arrayQuantidade = [1, 55, 23, 47, 56, 11]
+
+
+print(arrayQuantidade.count)
